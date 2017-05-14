a9c7643dd9adf12a6e6f0143647b54e6cf85a3ae
Harsh0 <harsh.singhal040@gmail.com>
2017-05-14 13:57:47 +0530

    added comment

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var path = require('path');
 var config = path.join(__dirname,"config.json");
 http.createServer(function(req,res){
   console.log("new req to "+req.url);
-  var content = fs.readFileSync(config);
+  var content = fs.readFileSync(config);//reading config file through fileSystem allowing change on the fly in config.json
   content = JSON.parse(String(content));
   var key = Object.keys(content);
   if(key.indexOf(req.url)>-1){
     var file = content[key[key.indexOf(req.url)]].path;
     var filepath;
-    if(content[key[key.indexOf(req.url)]].relative){
+    if(content[key[key.indexOf(req.url)]].relative){//if relative file exist in same directory else the absolute path will be there
       filepath = path.join(__dirname,file);
     }else{
       filepath = file;
@@ -21,11 +21,11 @@ http.createServer(function(req,res){
       "Content-Type":"application/octet-stream",
       "Content-Length":stat.size,
       "Content-Disposition":'filename="'+path.basename(filepath)+'"'
-    })
-    readStream.pipe(res);
+    });//sending response header
+    readStream.pipe(res);//piping readStream to response
   }else{
     res.writeHead(404,{"Content-Type":"text/plain"});
-    res.end('file not found');
+    res.end('file not found');//error no route found
   }
 }).listen(80,function(){
   console.log('file server has started');
