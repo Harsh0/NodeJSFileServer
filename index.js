73e7c93bd14369d11421f59838a8c50c587a4631
Harsh0 <harsh.singhal040@gmail.com>
2017-06-25 11:50:33 +0530

    added runTimeUpdateFlag to configure accotdingly

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var config = path.join(__dirname,"config.json");
-var port = process.argv[2]||8080;
-port = +port;
-http.createServer(function(req,res){
-  var reqLog = req.method+' '+req.url+'\t'+(new Date().toString());
-  fs.appendFile('requests.log', reqLog+'\n', (err) => {
-    if (err) throw err;
-    console.log(reqLog);
-  });
-  //add log to log file
-  var content = fs.readFileSync(config);//reading config file through fileSystem allowing change on the fly in config.json
-  content = JSON.parse(String(content));
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+let config = path.join(__dirname,"config.json");
+let port = process.argv[2]||8080;port = +port;
+let getJson = (config,runTimeJsonUpdateFlag)=>{
+  if(runTimeJsonUpdateFlag){//if true updated json content will be passed else content at starting time of server
+    var content = fs.readFileSync(config);//reading config file through fileSystem allowing change on the fly in config.json
+    return JSON.parse(String(content));
+  }else{
+    return require(config); 
+  }
+}
+http.createServer((req,res)=>{
+  content = getJson(config,false);
   var key = Object.keys(content);
   if(key.indexOf(req.url)>-1){
     var file = content[key[key.indexOf(req.url)]].path;
@@ -22,6 +22,12 @@ http.createServer(function(req,res){
     }else{
       filepath = file;
     }
+    let reqLog = req.method+' '+req.url+'\t'+filepath+'\t'+(new Date().toString());
+    //add log to log file
+    fs.appendFile('requests.log', reqLog+'\n', (err) => {
+      if (err) throw err;
+      console.log(reqLog);
+    });
     var stat = fs.statSync(filepath);
     var readStream = fs.createReadStream(filepath);
     res.writeHead(200,{
@@ -34,6 +40,4 @@ http.createServer(function(req,res){
     res.writeHead(404,{"Content-Type":"text/plain"});
     res.end('file not found');//error no route found
   }
-}).listen(port,function(){
-  console.log('File server has started at port '+port);
-})
+}).listen(port,()=>console.log('File server has started at port '+port));
