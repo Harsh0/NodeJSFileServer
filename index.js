faa0995af2246948485cc7022e56231f57bcb3dc
Harsh0 <harsh.singhal1@wipro.com>
2017-06-30 10:40:07 +0530

    added uploading

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const formidable = require('formidable');
 let config = path.join(__dirname,"config.json");
 let port = process.argv[2]||8080;port = +port;
 let getJson = (config,runTimeJsonUpdateFlag)=>{
@@ -8,13 +9,39 @@ let getJson = (config,runTimeJsonUpdateFlag)=>{
     var content = fs.readFileSync(config);//reading config file through fileSystem allowing change on the fly in config.json
     return JSON.parse(String(content));
   }else{
-    return require(config); 
+    return require(config);
   }
 }
+let static = {'.html':'text/html','.js':'text/javascript','.css':'text/css'};
+let staticKey = Object.keys(static);
 http.createServer((req,res)=>{
   content = getJson(config,false);
   var key = Object.keys(content);
-  if(key.indexOf(req.url)>-1){
+  if(req.url=='/uploadFile'){
+    var newForm = formidable.IncomingForm();
+    newForm.keepExtensions = true;
+    let tmpFile,nFile;
+    newForm.parse(req,function(err,fields,files){
+      tmpFile = files.upload.path;
+      nFile = __dirname+'/uploads'+'/'+files.upload.name;
+      res.writeHead(200,{
+        'Content-type':'text/plain'
+      });
+      res.end();
+    });
+    newForm.on('end',function(){
+      fs.rename(tmpFile,nFile,function(){
+        console.log('File uploaded successfully : '+path.basename(nFile));
+      });
+      reqLog = req.method+' '+req.url+'\t'+nFile+'\t'+(new Date().toString());
+      //add log to log file
+      fs.appendFile('requests.log', reqLog+'\n', (err) => {
+        if (err) throw err;
+        console.log(reqLog);
+      });
+    });
+  }
+  else if(key.indexOf(req.url)>-1){
     var file = content[key[key.indexOf(req.url)]].path;
     var filepath;
     if(content[key[key.indexOf(req.url)]].relative){//if relative file exist in same directory else the absolute path will be there
@@ -30,11 +57,19 @@ http.createServer((req,res)=>{
     });
     var stat = fs.statSync(filepath);
     var readStream = fs.createReadStream(filepath);
-    res.writeHead(200,{
-      "Content-Type":"application/octet-stream",
-      "Content-Length":stat.size,
-      "Content-Disposition":'filename="'+path.basename(filepath)+'"'
-    });//sending response header
+    if(staticKey.indexOf(path.extname(filepath))>-1){
+      //serve static web page to upload
+      res.writeHead(200,{
+        "Content-Type":static[path.extname(filepath)],
+      });//sending response header
+    }else{
+      //serve headers for file
+      res.writeHead(200,{
+        "Content-Type":"application/octet-stream",
+         "Content-Length":stat.size,
+         "Content-Disposition":'filename="'+path.basename(filepath)+'"'
+      });//sending response header
+    }
     readStream.pipe(res);//piping readStream to response
   }else{
     res.writeHead(404,{"Content-Type":"text/plain"});
