5197fb69626500d3d91e3d691ab891a76a46705d
Harsh Singhal <hsnghal@amazon.com>
2020-06-25 13:53:54 +0530

    updated

--- index.js
+++ index.js
@@ -2,86 +2,96 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const formidable = require('formidable');
-let config = path.join(__dirname,"config.json");
-let port = process.argv[2]||8080;port = +port;
-let getJson = (config,runTimeJsonUpdateFlag)=>{
-  if(runTimeJsonUpdateFlag){//if true updated json content will be passed else content at starting time of server
-    var content = fs.readFileSync(config);//reading config file through fileSystem allowing change on the fly in config.json
-    return JSON.parse(String(content));
-  }else{
-    return require(config);
-  }
+
+const uploadPath = './uploads/';
+const static = { '.html': 'text/html', '.js': 'text/javascript', '.css': 'text/css', '.pdf': 'application/pdf' };
+const mapping = {
+  '/': './static/index.html'
 }
-let static = {'.html':'text/html','.js':'text/javascript','.css':'text/css','.pdf':'application/pdf'};
-let staticKey = Object.keys(static);
-http.createServer((req,res)=>{
-  content = getJson(config,false);
-  var key = Object.keys(content);
-  //file upload route
-  if(req.url=='/uploadFile'){
-    if(!fs.existsSync('uploads')){
-      //make directory
-      fs.mkdir('uploads');
-    }
-    var newForm = formidable.IncomingForm();
-    newForm.keepExtensions = true;
-    let tmpFile,nFile;
-    newForm.parse(req,function(err,fields,files){
-      tmpFile = files.upload.path;
-      nFile = ((content['/upload'].uploadPath)||(__dirname+'/uploads/'))+files.upload.name;
-      res.writeHead(200,{
-        'Content-type':'text/plain'
-      });
-      res.end();
-    });
-    newForm.on('end',function(){
-      let rs = fs.createReadStream(tmpFile);
-      let ws = fs.createWriteStream(nFile);
-      rs.pipe(ws);
-      rs.on('end',function(){
-        //delete tmpFile
-        fs.unlink(tmpFile);
-      })
-      reqLog = req.method+' '+req.url+'\t'+nFile+'\t'+(new Date().toString());
-      //add log to log file
-      fs.appendFile('requests.log', reqLog+'\n', (err) => {
-        if (err) throw err;
-        console.log(reqLog);
-      });
-    });
+let port = process.argv[2] || 8080; port = +port;
+
+const uploadFile = (req, res) => {
+  if (!fs.existsSync(uploadPath)) {
+    //make directory
+    fs.mkdirSync(uploadPath);
   }
-  else if(key.indexOf(req.url)>-1){
-    var file = content[key[key.indexOf(req.url)]].path;
-    var filepath;
-    if(content[key[key.indexOf(req.url)]].relative){//if relative file exist in same directory else the absolute path will be there
-      filepath = path.join(__dirname,file);
-    }else{
-      filepath = file;
-    }
-    let reqLog = req.method+' '+req.url+'\t'+filepath+'\t'+(new Date().toString());
+  var newForm = formidable.IncomingForm();
+  newForm.keepExtensions = true;
+  let tmpFile, nFile;
+  newForm.parse(req, function (err, fields, files) {
+    tmpFile = files.upload.path;
+    nFile = uploadPath + files.upload.name;
+    res.writeHead(200, {
+      'Content-type': 'text/plain'
+    });
+    res.end();
+  });
+  newForm.on('end', function () {
+    let rs = fs.createReadStream(tmpFile);
+    let ws = fs.createWriteStream(nFile);
+    rs.pipe(ws);
+    rs.on('end', function () {
+      //delete tmpFile
+      fs.unlinkSync(tmpFile);
+    })
+    reqLog = req.method + ' ' + req.url + '\t' + nFile + '\t' + (new Date().toString());
     //add log to log file
-    fs.appendFile('requests.log', reqLog+'\n', (err) => {
+    fs.appendFile('requests.log', reqLog + '\n', (err) => {
       if (err) throw err;
       console.log(reqLog);
     });
-    var stat = fs.statSync(filepath);
-    var readStream = fs.createReadStream(filepath);
-    if(staticKey.indexOf(path.extname(filepath))>-1){
-      //serve static web page to upload
-      res.writeHead(200,{
-        "Content-Type":static[path.extname(filepath)],
-      });//sending response header
-    }else{
-      //serve headers for file
-      res.writeHead(200,{
-        "Content-Type":"application/octet-stream",
-         "Content-Length":stat.size,
-         "Content-Disposition":'filename="'+path.basename(filepath)+'"'
-      });//sending response header
-    }
-    readStream.pipe(res);//piping readStream to response
-  }else{
-    res.writeHead(404,{"Content-Type":"text/plain"});
-    res.end('file not found');//error no route found
+  });
+}
+
+const downloadFile = (req, res) => {
+  let file = req.url;
+  if(mapping[file]) {
+    file = mapping[file];
+  } else {
+    file = '.' + file;
   }
-}).listen(port,()=>console.log('File server has started at port '+port));
+  if (!fs.existsSync(file)) {
+    file = uploadPath + file.substring(1);
+    if (!fs.existsSync(file)) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end('file not found');//error no route found
+      return
+    }
+  }
+  let reqLog = req.method + ' ' + req.url + '\t' + file + '\t' + (new Date().toString());
+  //add log to log file
+  fs.appendFile('requests.log', reqLog + '\n', (err) => {
+    if (err) throw err;
+    console.log(reqLog);
+  });
+  const stat = fs.statSync(file);
+  const headers = {
+    "Content-Type": "application/octet-stream",
+    "Content-Length": stat.size,
+    "Content-Disposition": 'filename="' + path.basename(file) + '"'
+  }
+  if (static[path.extname(file)]) {
+    headers['Content-Type'] = static[path.extname(file)]
+  }
+  res.writeHead(200, headers);
+  const readStream = fs.createReadStream(file);
+  readStream.pipe(res);  
+}
+
+const errorHandling = (err, res) => {
+  res.writeHead(500, { "Content-Type": "text/plain" });
+  res.write('Internal Error occured\n');//error no route found
+  res.end(err);
+}
+
+http.createServer((req, res) => {
+  try {
+    if (req.url == '/uploadFile') {
+      uploadFile(req, res);
+    } else {
+      downloadFile(req, res);
+    }  
+  } catch (error) {
+    errorHandling(error, res);
+  }
+}).listen(port, () => console.log('File server has started at port ' + port));
